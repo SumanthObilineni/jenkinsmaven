ff45ebf4ca3cbb5827333be4df0a575808b7359d
SumanthObilineni <obilinenisumanth357@gmail.com>
2024-10-09 10:36:03 -0400

    adding project to git

---
+++ jenkinsmaven/src/main/java/maven/demo/app.java
@@ -0,0 +1,11 @@
+package maven.demo;
+
+public class app {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+
+	}
+
+}
---
+++ jenkinsmaven/src/test/java/maven/demo/apptest.java
@@ -0,0 +1,12 @@
+package maven.demo;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class apptest{
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
+
